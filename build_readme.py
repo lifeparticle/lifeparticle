f26083f28a90cfb5c0b0bcc85ec96f893a7111cb
lifeparticle <mahbubzaman17@gmail.com>
2020-07-18 17:29:39 +1000

    blog post added

---
+++ build_readme.py
@@ -0,0 +1,46 @@
+import requests
+import pathlib
+import json
+import sys
+import re
+import os
+
+root = pathlib.Path(__file__).parent.resolve()
+
+def replace_chunk(content, marker, chunk, inline=False):
+    r = re.compile(
+        r"<!\-\- {} starts \-\->.*<!\-\- {} ends \-\->".format(marker, marker),
+        re.DOTALL,
+    )
+    if not inline:
+        chunk = "\n{}\n".format(chunk)
+    chunk = "<!-- {} starts -->{}<!-- {} ends -->".format(marker, chunk, marker)
+    return r.sub(chunk, content)
+
+def fetch_blog_posts():
+	result = []
+	response = requests.get("https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@lifeparticle")
+	if response.status_code == 200:
+		posts = json.loads(response.text)["items"]
+		for post in posts:
+			if len(post["categories"]) != 0:
+				result.append(post)
+	elif response.status_code == 404:
+		print('Not Found: ') + link
+	return result
+
+if __name__ == "__main__":
+	readme = root / "README.md"
+
+	posts = fetch_blog_posts()
+	if len(posts) != 0:
+		readme_contents = readme.open().read()
+		rewritten = readme_contents
+
+		posts_md = "\n".join(
+			["* [{title}]({link}) - {pubDate}".format(**post) for post in posts]
+		)
+		print (posts_md)
+		rewritten = replace_chunk(rewritten, "blog", posts_md)
+
+		readme.open("w").write(rewritten)
