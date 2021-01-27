99ac67ed6d15ebeb40d9501ce88331d0e0e040f0
lifeparticle <mahbubzaman17@gmail.com>
2021-01-27 18:01:27 +1100

    random

--- build_readme.py
+++ build_readme.py
@@ -15,7 +15,8 @@ def update_programmer_humor_img(name):
 	client_secret = os.environ['CLIENT_SECRET']
 	client = ImgurClient(client_id, client_secret)
 	items = client.subreddit_gallery(name, sort='top', window='week', page=0)
-	return '<a href="{}" target="_blank"><img height="400" width="400" src="{}"></a>'.format(items[0].link, items[0].link)
+	item = random.choice(items)
+	return '<a href="{}" target="_blank"><img height="400" width="400" src="{}"></a>'.format(item.link, item.link)
 
 def replace_chunk(content, marker, chunk, inline=False):
 	# build the regular expression pattern, DOTALL will match any character, including a newline
