77b8659fba50d5f11fcb208b228d40d0f72ecd5b
Santosh Bhandari <santoshvandari1@gmail.com>
2023-10-03 19:14:36 +0545

    Updating and Refactoring the Code

--- build_readme.py
+++ build_readme.py
@@ -8,16 +8,19 @@ import sys
 import re
 import os
 
-# https://help.medium.com/hc/en-us/articles/214874118-RSS-feeds
-
-root = pathlib.Path(__file__).parent.resolve()
+# Constants
+ROOT_DIR = pathlib.Path(__file__).parent.resolve()
+CLIENT_ID = os.environ['CLIENT_ID']
+CLIENT_SECRET = os.environ['CLIENT_SECRET']
+PROGRAMMER_HUMOR_LINK = "https://imgur.com/r/ProgrammerHumor/{}"
+MEDIUM_RSS_LINK = "https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@lifeparticle"
 
 def create_imgur_link(id, link):
-    return '<a href="https://imgur.com/r/ProgrammerHumor/{}"><img max-height="400" width="350" src="{}"></a>'.format(id, link)
+    return f'<a href="{PROGRAMMER_HUMOR_LINK.format(id)}"><img max-height="400" width="350" src="{link}"></a>'
 
 def update_programmer_humor_img(name):
     try:
-        client = ImgurClient(os.environ['CLIENT_ID'], os.environ['CLIENT_SECRET'])
+        client = ImgurClient(CLIENT_ID, CLIENT_SECRET)
         credits = client.credits
         if credits["UserRemaining"] > 0:
             items = client.subreddit_gallery(name, sort="top", window="week", page=0)
@@ -25,7 +28,7 @@ def update_programmer_humor_img(name):
                 if not item.link.endswith((".mp4", ".gif")):
                     return create_imgur_link(item.id, item.link)
         else:
-            print("Not enough credits remaining to make request.")
+            print("Not enough credits remaining to make the request.")
             
         return create_imgur_link("SV767tT", "SV767tT")
 
@@ -33,19 +36,14 @@ def update_programmer_humor_img(name):
         print(e.error_message)
 
 def replace_chunk(content, marker, chunk, inline=False):
-    # build the regular expression pattern, DOTALL will match any character, including a newline
-    r = re.compile(
-        r"<!-- {} starts -->.*<!-- {} ends -->".format(marker, marker),
+    pattern = re.compile(
+        f"<!-- {marker} starts -->.*<!-- {marker} ends -->",
         re.DOTALL,
     )
-    # add newline before and after
     if not inline:
-        chunk = "\n{}\n".format(chunk)
-    # build the final chunk by adding comments before and after the chunk
-    chunk = "<!-- {} starts -->{}<!-- {} ends -->".format(marker, chunk, marker)
-    # replace matched string using pattern provided with the chunk
-    return r.sub(chunk, content)
-
+        chunk = f"\n{chunk}\n"
+    chunk = f"<!-- {marker} starts -->{chunk}<!-- {marker} ends -->"
+    return pattern.sub(chunk, content)
 
 def fetch_blog_posts(link):
     result = []
@@ -53,38 +51,22 @@ def fetch_blog_posts(link):
     if response.status_code == 200:
         posts = json.loads(response.text)["items"]
         for post in posts:
-            # skip the comments
             if len(post["categories"]) != 0:
                 post["pubDate"] = post["pubDate"].split()[0]
                 result.append(post)
     elif response.status_code == 404:
-        print("Not Found: ") + link
+        print(f"Not Found: {link}")
     return result
 
-
 if __name__ == "__main__":
-    readme = root / "README.md"
-
+    readme = ROOT_DIR / "README.md"
     readme_contents = readme.open().read()
     rewritten = readme_contents
-    rewritten = replace_chunk(
-        rewritten,
-        "programmer_humor_img",
-        update_programmer_humor_img("ProgrammerHumor"),
-    )
+    rewritten = replace_chunk(rewritten, "programmer_humor_img", update_programmer_humor_img("ProgrammerHumor"))
 
-    posts = fetch_blog_posts(
-        "https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@lifeparticle"
-    )
-    if len(posts) != 0:
-        # markdown formatting
-        posts_md = "\n".join(
-            [
-                "* [{title}]({link}) <br/> <sub>{pubDate}</sub>".format(**post)
-                for post in posts
-            ]
-        )
-
+    posts = fetch_blog_posts(MEDIUM_RSS_LINK)
+    if posts:
+        posts_md = "\n".join([f"* [{post['title']}]({post['link']}) <br/> <sub>{post['pubDate']}</sub>" for post in posts])
         rewritten = replace_chunk(rewritten, "blog", posts_md)
 
     readme.open("w").write(rewritten)
